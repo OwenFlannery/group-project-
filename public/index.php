89a16e3c6bcfd55ea8fd74c4cbd77aeedefcd7db
OwenFlannery <owen.flannery22@gmail.com>
2017-04-25 11:52:25 +0100

    ss

--- public/index.php
+++ public/index.php
@@ -1,89 +1,139 @@
 <?php
 session_start();
 $connect = mysqli_connect("localhost", "root", "root", "db");
+if(isset($_POST["add_to_cart"]))
+{
+    if(isset($_SESSION["shopping_cart"]))
+    {
+        $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+        if(!in_array($_GET["id"], $item_array_id))
+        {
+            $count = count($_SESSION["shopping_cart"]);
+            $item_array = array(
+                'item_id'=> $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'item_price'=> $_POST["hidden_price"],
+                'item_quantity'=>$_POST["quantity"]
+            );
+            $_SESSION["shopping_cart"][$count] = $item_array;
+        }
+        else
+        {
+            echo '<script>alert("Item Already Added")</script>';
+            echo '<script>window.location="index.php"</script>';
+        }
+    }
+    else
+    {
+        $item_array = array(
+            'item_id'=>$_GET["id"],
+            'item_title'=>$_POST["hidden_name"],
+            'item_price'=>$_POST["hidden_price"],
+            'item_quantity'=>$_POST["quantity"]
+        );
+        $_SESSION["shopping_cart"][0] = $item_array;
+    }
+}
+if(isset($_GET["action"]))
+{
+    if($_GET["action"] == "delete")
+    {
+        foreach($_SESSION["shopping_cart"] as $keys => $values)
+        {
+            if($values["item_id"] == $_GET["id"])
+            {
+                unset($_SESSION["shopping_cart"][$keys]);
+                echo '<script>alert("Item Removed")</script>';
+                echo '<script>window.location="index.php"</script>';
+            }
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 
-    <link rel="stylesheet" type="text/css" href="css/page_layout.css">
-    <link rel="stylesheet" type="text/css" href="css/slideshow.css">
-
-    <script>
-        var slideIndex = 1;
-        showSlides(slideIndex);
-
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-        }
-    </script>
 </head>
 <body>
-<header>
-    <!***************************************search bar and nav bar ******************************>
-    <form method="post" action="search.php?go" id="search_form">
-        <input type="text" name="search_bar" placeholder="search book titles">
-        <input type="submit" name="submit" value="search">
-    </form>
-    <nav>
-        <ul class="nav_ul">
-            <li class="nav_li"><a href="index.php" data-toggle="dropdown" title="Home">Home</a></li>
-
-            <li class="nav_li"><a href="rec.php" data-toggle="dropdown" title="Recreational Books">Recreational Books</a></li>
-            <li class="nav_li"><a href="trade.php" data-toggle="dropdown" title="Tradable Books">Tradable Books</a></li>
-            <li class="nav_li"><a href="login.php" data-toggle="dropdown" title="login/new user">login/new user</a></li>
-            <li class="nav_li"><a href="contact_us.php" data-toggle="dropdown" title="Contact Us">Contact Us</a></li>
-        </ul>
-    </nav>
-</header>
-    <!--***********************************SLIDE SHOW of BOOKS*********************-->
-    <div class="slideshow-container">
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="images/lotr_fellowship.jpg" style="width:50%" height="50%">
-            <div class="text">lotr fellowship of the ring</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="images/lotr_twin.jpg" style="width:50%" height="50%">
-            <div class="text">lotr the twin towers</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="images/lotr_return.jpg" style="width:50%" height="50%">
-            <div class="text">lotr the return of the king</div>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+<!--********************************search bar*********************************-->
+<p>search titles</p>
+<form method="post" action="search.php?go" id="search_form">
+    <input type="text" name="search_bar" placeholder="search book titles">
+    <input type="submit" name="submit" value="search">
+</form>
+<!--*****************************end of form ************************************-->
+<br />
+<div class="container" style="width:700px;">
+    <h3 align="center">Simple PHP Mysql Shopping Cart</h3><br />
+    <?php
+    $query = "SELECT * FROM BOOKS ORDER BY PRODUCTID ASC";
+    $result = mysqli_query($connect, $query);
+    if(mysqli_num_rows($result) > 0)
+    {
+        while($row = mysqli_fetch_array($result))
+        {
+            ?>
+            <div class="col-md-4">
+                <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+                        <img src="<?php echo $row["image"]; ?>" class="img-responsive" /><br />
+                        <h4 class="text-info"><?php echo $row["name"]; ?></h4>
+                        <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
+                        <input type="text" name="quantity" class="form-control" value="1" />
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+                        <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                    </div>
+                </form>
+            </div>
+            <?php
+        }
+    }
+    ?>
+    <div style="clear:both"></div>
+    <br />
+    <h3>Order Details</h3>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <tr>
+                <th width="40%">Item Name</th>
+                <th width="10%">Quantity</th>
+                <th width="20%">Price</th>
+                <th width="15%">Total</th>
+                <th width="5%">Action</th>
+            </tr>
+            <?php
+            if(!empty($_SESSION["shopping_cart"]))
+            {
+                $total = 0;
+                foreach($_SESSION["shopping_cart"] as $keys => $values)
+                {
+                    ?>
+                    <tr>
+                        <td><?php echo $values["item_name"]; ?></td>
+                        <td><?php echo $values["item_quantity"]; ?></td>
+                        <td>$ <?php echo $values["item_price"]; ?></td>
+                        <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
+                        <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+                    </tr>
+                    <?php
+                    $total = $total + ($values["item_quantity"] * $values["item_price"]);
+                }
+                ?>
+                <tr>
+                    <td colspan="3" align="right">Total</td>
+                    <td align="right">$ <?php echo number_format($total, 2); ?></td>
+                    <td></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
     </div>
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
-    <br>
+</div>
+<br />
 </body>
 <footer>
 
---
+++ src/upload.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: owen
+ * Date: 24/04/2017
+ * Time: 13:21
+ */
+class upload
+{
+
+}
